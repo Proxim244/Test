b0e51ffb33cfa63613dc9c821a22241db9afd952
fcd <fcd@172.16.1.133>
2017-12-11 16:13:30 +0100

    c'est mon commit sans humour, mais je vais me refaire

--- echoBonjour.php
+++ echoBonjour.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       echo "bonjour monsieur";
+       echo "bonjour monsieur, j'ai pas de blague";
         ?>
     </body>
 </html>
